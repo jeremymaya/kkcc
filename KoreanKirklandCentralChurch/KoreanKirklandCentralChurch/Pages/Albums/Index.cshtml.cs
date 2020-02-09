eca23df399530fe02b501e5a727b3a83b85b02d5
Kyungrae Kim <jeremiah.kim@hotmail.com>
2020-02-08 22:47:23 -0800

    README.md updated with log and future works

--- KoreanKirklandCentralChurch/KoreanKirklandCentralChurch/Pages/Albums/Index.cshtml.cs
+++ KoreanKirklandCentralChurch/KoreanKirklandCentralChurch/Pages/Albums/Index.cshtml.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace KoreanKirklandCentralChurch.Pages.Album
+namespace KoreanKirklandCentralChurch.Pages.Albums
 {
     public class IndexModel : PageModel
     {
