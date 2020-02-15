e73e273947eb77a14d8a8d1f75db783dd5959edf
Kyungrae Kim <jeremiah.kim@hotmail.com>
2020-02-14 19:25:43 -0800

    semon class completed

---
+++ KoreanKirklandCentralChurch/KoreanKirklandCentralChurch/Models/Sermon.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KoreanKirklandCentralChurch.Models
+{
+    public class Sermon
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "설교제목")]
+        public string Title { get; set; }
+
+        [Required]
+        [Display(Name = "설교제목")]
+        public string Speaker { get; set; }
+
+        [Required]
+        [Display(Name = "설교날짜")]
+        public DateTime Date { get; set; }
+
+        [Required]
+        [Display(Name = "설교링크")]
+        public string SermonLink { get; set; }
+    }
+}
