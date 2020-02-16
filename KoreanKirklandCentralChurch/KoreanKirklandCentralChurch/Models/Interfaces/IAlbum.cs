642f83d4e348f336162d034ff533ca1de3776b1b
Kyungrae Kim <jeremiah.kim@hotmail.com>
2020-02-15 19:45:03 -0800

    Interface for album service completed

---
+++ KoreanKirklandCentralChurch/KoreanKirklandCentralChurch/Models/Interfaces/IAlbum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KoreanKirklandCentralChurch.Models.Interfaces
+{
+    interface IAlbum
+    {
+        Task CreateAlbumAsync(Sermon sermon);
+
+        Task<Sermon> GetAlbumByIdAsync(int id);
+
+        Task<IList<Sermon>> GetAlbumsAsync();
+
+        Task UpdateAlbumAsync(Sermon sermon);
+
+        Task DeleteAlbumAsync(int id);
+    }
+}
