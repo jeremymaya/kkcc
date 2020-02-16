c5d041d896079996d5f569b0c4d106e0bfaf18b9
Kyungrae Kim <jeremiah.kim@hotmail.com>
2020-02-15 21:18:34 -0800

    homepage updated to dynamically render from database

--- KoreanKirklandCentralChurch/KoreanKirklandCentralChurch/Pages/Index.cshtml.cs
+++ KoreanKirklandCentralChurch/KoreanKirklandCentralChurch/Pages/Index.cshtml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using KoreanKirklandCentralChurch.Models;
+using KoreanKirklandCentralChurch.Models.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +11,19 @@ namespace KoreanKirklandCentralChurch.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly ISermon _sermon;
+
+        public IndexModel(ISermon sermon)
         {
+            _sermon = sermon;
+        }
+
+        public Sermon LatestSermon { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            LatestSermon = await _sermon.GetLatestSermonAsync();
+
         }
     }
 }
