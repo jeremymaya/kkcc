433f71f4054f66c4efa0ece1ef2fcf588b737bae
Kyungrae Kim <jeremiah.kim@hotmail.com>
2020-02-17 15:26:21 -0800

    details sermon page for admin completed

---
+++ KoreanKirklandCentralChurch/KoreanKirklandCentralChurch/Pages/Admin/Sermons/Details.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KoreanKirklandCentralChurch.Models;
+using KoreanKirklandCentralChurch.Models.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KoreanKirklandCentralChurch.Pages.Admin.Sermons
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly ISermon _sermon;
+
+        public DetailsModel(ISermon sermon)
+        {
+            _sermon = sermon;
+        }
+
+        public Sermon Sermon { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Sermon = await _sermon.GetSermonByIdAsync(id);
+
+            if (Sermon == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
